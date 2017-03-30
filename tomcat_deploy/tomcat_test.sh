40f4526086ff7e299e94b45ebf621cf36ebc04a8
jmlrt <julien.mailleret@weekendesk.fr>
2017-03-30 16:52:37 +0200

    pass variable to ansible to be able to deploy different configurations by environments

--- tomcat_deploy/tomcat_test.sh
+++ tomcat_deploy/tomcat_test.sh
@@ -3,10 +3,22 @@
 # Check that tomcat is running well
 # Run this script after tomcat installation
 
+export ENVIRONMENT=$1
+
+usage() {
+    echo "Usage:  $0 <ENVIRONMENT>"
+    exit 1
+}
+
+if [ $# -ne 1 ]
+then
+    usage
+fi
+
 echo ""
 echo "Deploy Tomcat using Ansible"
 echo "---------------------------"
-ansible-playbook /data/deploy_tomcat.yml
+ansible-playbook --extra-vars="env=${ENVIRONMENT}" /data/deploy_tomcat.yml
 echo ""
 
 echo "Check java process and parameters"
